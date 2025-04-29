84545d34cef11fe61ca877f3fd8432c2b5bdcca0
JOVANKA26 <mjovankaslaviawijaya_2428250079@mhs.mdp.ac.id>
2025-04-29 15:11:06 +0700

    latihan

---
+++ latihan/resources/views/berita.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h4>Ini Adalah Halaman Berita</h4>
+    ID: <?php  echo $id; ?> 
+    Title: <?php echo $title; ?>
+</div>
--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Pest\Plugins\Profile;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/profil', function() {
+    return view( 'profil');
 });
+
+Route::get('/berita/{id}/{title}', function ($id, $title = NULL) {
+    return view('berita', ['id' => '$id', 'title'=> $title ]);
+});
+
