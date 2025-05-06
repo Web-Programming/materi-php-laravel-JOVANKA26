253a2c33c8e67cb6a33cb4d30a7071a7f3874468
JOVANKA26 <mjovankaslaviawijaya_2428250079@mhs.mdp.ac.id>
2025-05-06 07:34:39 +0700

    latihan

--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Pest\Plugins\Profile;
+use App\Http\Controllers\MhsApiController;
+use Illuminate\Support\Facades\Route;  
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home');
 });
 
@@ -15,4 +16,23 @@ Route::get('/berita/{id}/{title}', function ($id, $title = NULL) {
     return view('berita', ['id' => '$id', 'title'=> $title ]);
 });
 
-#
+Route::get('/fakultas', function () {
+    // return view('fakultas.index', ["ilkom" => "Fakultas Ilmu Komputer dan Rekayasa"]);
+    // return view('fakultas.index', ["fakultas" => ["Fakultas Ilmu Komputer dan Rekayasa", "Fakultas Ilmu Ekonomi"]]);
+    // return view('fakultas.index')->with("fakultas", ["Fakultas Ilmu Komputer dan Rekayasa", "Fakultas Ilmu Ekonomi"]);
+
+    $kampus = "Universitas Multi Data Palembang";
+    // $fakultas = [];
+    $fakultas = ["Fakultas Ilmu Komputer dan Rekayasa", "Fakultas Ilmu Ekonomi"];
+
+    return view('fakultas.index', compact('fakultas', 'kampus'));
+});
+
+Route::get('/materi/index', 
+[materi::class, 'index']);
+
+Route::resource('prodi',
+ProdiController::class);
+
+Route::apiResource('api/mhs', 
+MhsApiController::class);
