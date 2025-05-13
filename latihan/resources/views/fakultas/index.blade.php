d4149d620dbb8454737eeb946857f1880484f8f3
JOVANKA26 <mjovankaslaviawijaya_2428250079@mhs.mdp.ac.id>
2025-05-13 13:02:50 +0700

    latihan

--- latihan/resources/views/fakultas/index.blade.php
+++ latihan/resources/views/fakultas/index.blade.php
@@ -1,34 +1,50 @@
-@include('layout.header', ['title' => 'Halaman Fakultas'])
-
-<h2>Fakultas</h2>
-<ul>
-    @if (count($fakultas) > 0)
-        @foreach ($fakultas as $item)
-            <li> {{ $item }} </li>
-        @endforeach
-    @else
-        <li>Belum ada data</li>
-    @endif
-</ul>
-
-@include('layout.footer')
-
-{{--@include('layout.header', ['title' => 'Halaman Fakultas'])--}}
-
-@extends('layout.master')
-@section('title', 'Halaman Fakultas')
+@extends('latihanLayout.master')
 
 @section('content')
-<h2>Fakultas</h2>
-<ul>
-    @if (count($fakultas) > 0)
-        @foreach ($fakultas as $item)
-            <li> {{ $item }} </li>
-        @endforeach
-    @else
-        <li>Belum ada data</li>
-    @endif
-</ul>
-@endsection
+    <div class="container mt-4">
+        <h3>Daftar Fakultas</h3>
 
-{{--@include('layout.footer')--}}
+        <a href="#" class="btn btn-primary mb-3">+ Tambah Fakultas</a>
+
+        @php
+            $fakultasList = [
+                (object) [
+                    'id' => 1,
+                    'nama' => 'Ilmu Komputer & Rekayasa',
+                    'deskripsi' => 'Fakultas yang membawahi jurusan ilmu komputer dan rekayasa'
+                ],
+                (object) [
+                    'id' => 2,
+                    'nama' => 'Ekonomi & Bisnis',
+                    'deskripsi' => 'Fakultas yang membawahi jurusan ekonomi dan bisnis'
+                ],
+            ];
+        @endphp
+
+        <table class="table table-bordered table-striped">
+            <thead class="thead-dark">
+                <tr>
+                    <th>No</th>
+                    <th>Nama Fakultas</th>
+                    <th>Deskripsi</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($fakultasList as $index => $fakultas)
+                    <tr>
+                        <td>{{ $index + 1 }}</td>
+                        <td>{{ $fakultas->nama }}</td>
+                        <td>{{ $fakultas->deskripsi }}</td>
+                        <td>
+                            <center>
+                                <a href="#" class="btn btn-info btn-sm">Detail</a>
+                                <button class="btn btn-danger btn-sm" onclick="return confirm('Hapus data ini?')">Hapus</button>
+                            </center>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
